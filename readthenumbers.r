900aba649f3f34f848227abd4c6863872d3e5f06
Camilo Strange <camilor@gmail.com>
2020-07-12 12:53:29 -0400

    Update readthenumbers.r
    reading data

--- readthenumbers.r
+++ readthenumbers.r
@@ -1,2 +1,9 @@
 library(tidyverse)
+library(lubridate)
 
+#read data
+dailycsv <- read_csv("./rawdata/daily.csv")
+
+datesintable <- c("date", "dateChecked", "lastModified")
+dailycsv %>% 
+  ymd(datesintable)
