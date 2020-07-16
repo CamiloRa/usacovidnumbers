1bf8caebc788494a6caa28f05ff8723033e0c486
Camilo Strange <camilor@gmail.com>
2020-07-16 18:47:52 -0400

    graph is now actual and rolling 14 days

--- readthenumbers.r
+++ readthenumbers.r
@@ -1,13 +1,20 @@
 library(tidyverse)
 library(lubridate)
-
+library(tibbletime)
 #read data
 dailycsv <- read_csv("./rawdata/daily.csv")
-
 datesintable <- c("date", "dateChecked", "lastModified")
-
 dailycsv$date <-  ymd(dailycsv$date)
+rolling_mean <- rollify(mean, window = 14 )
 
-ggplot (dailycsv, aes(x=date, y=deathIncrease)) +
-  geom_line()
+dailycsv <- dailycsv %>% mutate (
+  deathRolling = rolling_mean(deathIncrease)
+)
+  
+
+  
+ggplot (dailycsv, aes(x=date )) +
+  geom_line(aes(y=deathIncrease), color = "red") +
+  geom_line(aes(y=deathRolling), color  = "blue")
+
 
