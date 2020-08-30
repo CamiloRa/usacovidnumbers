095bf20994c36fac7ff67c6c730ee9924cf1154a
Camilo Strange <camilor@gmail.com>
2020-08-30 11:33:50 -0400

    ggplotting the file

--- readthenumbers.r
+++ readthenumbers.r
@@ -11,25 +11,22 @@ dailycsv <- read_csv("./rawdata/daily.csv")
 datesintable <- c("date", "dateChecked", "lastModified")
 dailycsv$date <-  ymd(dailycsv$date)
 #dailycsv$state <-  as.factor(dailycsv$state)
-rolling_mean <- rollify(mean, window = 14 )
+rolling_mean14 <- rollify(mean, window = 14 )
+rolling_mean07 <- rollify(mean, window = 7 )
+rolling_mean03 <- rollify(mean, window = 3 )
 
 daily_rolling <- dailycsv %>% select("date",  "death", "positive", "deathIncrease")
 
 daily_rolling2 <- daily_rolling %>%
   mutate (
   deathRolling = rolling_mean(deathIncrease)
-) %>% 
-  group_by(date) %>% 
-  summarize(sum(positive), sum(death))
+)
 
 
 # plots -------------------------------------------------------------------
 
-ggplot (daily_rolling, aes(x=date )) +
-  geom_line(aes(y=deathIncrease), color = "red") +
-  ggtitle("Numbers and Rolling Average")
-
-ggplot (totalUSA, aes(x=date, y=deathIncrease)) +
+plot1 <- ggplot (daily_rolling2, aes(x=date, y= deathIncrease)) +
   geom_line(color = "red") +
   ggtitle("Numbers and Rolling Average")
 
+plot1
